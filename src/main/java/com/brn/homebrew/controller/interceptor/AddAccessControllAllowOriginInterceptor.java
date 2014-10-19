02819505180e7414317ed843060ff76856756d9e
jbrunodomingues <jbrunodomingues@hotmail.com>
2014-10-19 15:20:04 +0100

    CORS and some more refactoring

---
+++ src/main/java/com/brn/homebrew/controller/interceptor/AddAccessControllAllowOriginInterceptor.java
@@ -0,0 +1,19 @@
+package com.brn.homebrew.controller.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Bruno Domingues
+ */
+public class AddAccessControllAllowOriginInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        return true;
+    }
+}
--- src/main/java/com/brn/homebrew/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/brn/homebrew/service/impl/UserDetailsServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +20,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     private UserDao userDao;
 
+    @Transactional
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userDao.findUserByUserName(username);
